d1f68b021a227856302c7ee5e1d19570cfc42225
raheescv <raheescv1992@gmail.com>
2024-10-12 14:33:37 +0530

    netlif

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   modules: ["usebootstrap"],
   app: {
     head: {
-      title: "My Erp Nuxt App",
+      title: "My ERP Nuxt App",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
